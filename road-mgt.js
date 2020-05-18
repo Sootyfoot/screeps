0c89d1109e1c5c0ca17ca121f32be398e4325035
Sootyfoot <65080790+Sootyfoot@users.noreply.github.com>
2020-05-18 15:49:00 -0400

    Create road-mgt.js

---
+++ road-mgt.js
@@ -0,0 +1,20 @@
+/*
+function (loc)
+{
+  // if a road exists at loc, repair it
+  // else if a flag exists at loc, upvote it
+    // if flag upvote hits threshold, create construction site for road at loc and remove flag
+  // else if no construction site at loc, drop a flag and set its countdown timer
+  // else error
+}
+
+function (roadFlagArray)
+{
+  roadFlagArray.forEach(
+    roadFlagArray[i].memory.countdown -=;
+    if (roadFlagArray[i].memory.countdown <= 0)
+    {
+      // remove flag
+    }
+  );
+}
